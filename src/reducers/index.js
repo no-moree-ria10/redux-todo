47cca1f63c90d85ae01533c57a749dea6b059bdb
otaTakuma <otatakuma0714@gmail.com>
2017-05-03 11:19:26 +0900

    bug fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 
-const todo =(state, action)=> {
-    swich(action.type){
+const todo = (state, action) => {
+    switch (action.type) {
         case 'ADD_TODO':
         return{
             id: action.id,
-            text:text
+            text:action.text
         }
         default:
         return state
