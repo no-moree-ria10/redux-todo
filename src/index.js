cf94d742a93edb7cf2b7b843abad9a3ec2b1b4ef
otaTakuma <otatakuma0714@gmail.com>
2017-05-03 11:22:14 +0900

    ad store attribute

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+
 import todo from './reducers'
 
 import App from './components/App';
 import './index.css';
 
+const store = createStore(todo)
+
 render(
-        <Provider>
+        <Provider store={store} >
         <App />
         </Provider>,
     document.getElementById('root')
