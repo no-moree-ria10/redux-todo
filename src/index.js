758d4e2c6b699b95beacd98b9757c9fef7872978
otaTakuma <otatakuma0714@gmail.com>
2017-05-03 11:03:51 +0900

    Appの初期化

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux'
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render(
+        <Provider>
+        <App />
+        </Provider>,
+    document.getElementById('root')
 );
