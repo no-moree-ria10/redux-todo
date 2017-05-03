f5f77e0be0be171f23a2c6e4d82c1e715fdcc3d5
otaTakuma <otatakuma0714@gmail.com>
2017-05-03 11:15:08 +0900

    ad import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todo from './reducers'
+
 import App from './App';
 import './index.css';
 
