2f06d5d241d4fdec591c92f4b38788edb3a6183e
ria10 <otatakuma0714@gmail.com>
2017-05-21 10:21:35 +0900

    完了!

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,26 @@
 import React, { PropTypes } from 'react'
 
-const Link = ({ children, onClick }) => (
-  <a href="#">{children}</a>
-)
+const Link = ({ active, children, onClick }) => {
+    if (active) {
+        return <span>{children}</span>
+    }
+    return(
+        <a href="#"
+           onClick={(e)=>{
+               e.preventDefault()
+               onClick()
+          }}
+          >
+          {children}
+        </a>
+    )
+}
 
 Link.propTypes = {
-  children: PropTypes.node.isRequired
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired
 }
+
 
 export default Link
