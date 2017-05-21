237408244014a392d5a691be6444eadfc8db360d
ria10 <otatakuma0714@gmail.com>
2017-05-21 09:24:57 +0900

    bugfix:import文の追加

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import TodoList from '../components/TodoList'
+import { toggleTodo } from '../actions'
 
 const mapStateToProps = (state) => {
     return { todos: state.todos }
